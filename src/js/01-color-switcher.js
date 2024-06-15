eb9c8c6edd179d8b5453a7b36fc138557025dfab
Darina Khalina <darina.khalina@namecheap.com>
2024-06-15 19:06:24 +0300

    chore(hw2): add task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+const elements = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+const INTERVAL_DURATION = 1000;
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+elements.startBtn.addEventListener('click', () => {
+  elements.startBtn.disabled = true;
+  elements.stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DURATION);
+});
+
+elements.stopBtn.addEventListener('click', () => {
+  elements.startBtn.disabled = false;
+  elements.stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+});
